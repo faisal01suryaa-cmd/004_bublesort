7d1cf38bd17eaad892567148efcc771033d68229
faisal01suryaa <faisal01suryaa@gmail.com>
2026-03-05 15:29:41 +0700

    mendeklarasikan arraynya dan menampilkan untuk input pengguna, menyeting kurang dari atau sama dengan 20 dan menentukan array adalah 20

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -3,3 +3,21 @@
 #include <string>
 using namespace std;
 
+int arr[20]; //deklarasi variabel global array a dengan ukuran 20
+int n;       //deklarsi variabel global n untuk menyimpan banyaknya elemen 
+
+void input()
+{ //procedur utnuk input
+    int d;
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : "; //output kelayar
+        cin >> n;                                          //input dari pengguna
+        if (n <= 20)                                       //jika n kurang atau sama dengan
+            break;                                         //keluar dari loop
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
