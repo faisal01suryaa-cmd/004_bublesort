de6485cd90a4a87fe2d484e4575cb61fcdd8aa0e
faisal01suryaa <faisal01suryaa@gmail.com>
2026-03-05 15:35:14 +0700

    lenjutan dari prosedur input sebelumnya

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -20,4 +20,14 @@ void input()
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" <<(i+1) <<": ";
+        cin >> arr[i];
+    }
 }
