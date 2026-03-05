14bf7f1f897d8cf0dde69ef48ab36f4da27e8202
faisal01suryaa <faisal01suryaa@gmail.com>
2026-03-05 16:04:19 +0700

    menambahkan display program utama dan array

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -30,4 +30,70 @@ void input()
         cout << "data ke-" <<(i+1) <<": ";
         cin >> arr[i];
     }
+
+    
+    
+}
+
+void bubbleSortArray()
+{ //prosedur mengurutkan array dengan bubble sort
+   int pass = 1; //step1
+
+   do
+   {
+    for (int j = 0; j <= n - 1 - pass; j++)
+    { //step 2
+       if (arr[j] > arr[j + 1])
+       { //step3
+        int temp;
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+       }
+    }
+    pass += 1; //step4
+
+    cout << "\npass " << pass -1 << ": "; //number of pass
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " "; //menampilkan data pada number of pass
+    }
+    cout << endl;
+
+   } while (pass <= n -1); //step5
+   
+}
+void display()
+{
+    cout << endl;
+    cout << "==========" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================" << endl;
+    cout << endl;
+    for ( int j = 0; j < n; j++)
+    {
+        cout << arr[j]; //menampilkan array
+        if (j < n - 1)
+        {
+            cout <<"-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return;
+
 }
